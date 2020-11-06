74a60b3a2bb2abbc7b49fc3253d112a347c864af
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-11-06 13:23:39 +0100

    tset3

--- RFunction.R
+++ RFunction.R
@@ -33,18 +33,18 @@ rFunction = function(time_now=NULL, time_dur=NULL, data, ...) {
     datai_t <- datai[timestamps(datai)>time0 & timestamps(datai)<time_now]
     datai_t.df <- datai_t@data #as.data.frame(data)
     
-    if (length(datai_t)>0)
-    {
-      bb <- bbox(datai_t)+c(-0.1,-0.1,0.1,0.1)
-      m <- get_map(bb,maptype="terrain",source="osm")
-      g[[k]] <- ggmap(m)
+    #if (length(datai_t)>0)
+    #{
+    #  bb <- bbox(datai_t)+c(-0.1,-0.1,0.1,0.1)
+    #  m <- get_map(bb,maptype="terrain",source="osm")
+    #  g[[k]] <- ggmap(m)
     #    geom_path(data=datai_t.df,aes(x=location_long,y=location_lat),colour="orange") +
     #    geom_point(data=tail(datai_t.df),aes(x=location_long,y=location_lat),colour=2,size=2,pch=20) +
     #    labs(title = paste("individual:",ids[i])) +
     #    theme(plot.margin=grid::unit(c(2,2,2,2), "cm"))
-      k <- k+1
-        
-    } else logger.info(paste0("There are no locations available in the requested time window for individual ",ids[i]))
+    #  k <- k+1
+    #    
+    #} else logger.info(paste0("There are no locations available in the requested time window for individual ",ids[i]))
   }
 
   #gp  <- marrangeGrob(g, nrow = 1, ncol = 1)
